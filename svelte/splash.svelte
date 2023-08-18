b49fbd5c347a1e141afffa246db10436cd1bcfcf
n3k0lai <7215827+n3k0lai@users.noreply.github.com>
2023-08-18 18:05:12 -0400

    some svelte and assets

---
+++ svelte/splash.svelte
@@ -0,0 +1,8 @@
+
+<div class="splash waves flex flex-col md:flex-row-reverse md:justify-end md:items-end items-stretch justify-stretch">
+		
+    <h1 class="grow flex self-center justify-center align-middle text-6xl">
+        n3k0lai
+    </h1>
+    <div class="cat md:grow aspect-square"></div>
+</div>
